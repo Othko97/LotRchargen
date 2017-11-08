e691bd1edfdbb484efef950c4b4e8ab2b3591e9e
Othko97 <othko97@gmail.com>
2017-11-08 11:52:19 +0000

    Make translators functions

--- skilltranslator.py
+++ skilltranslator.py
@@ -1,13 +1,15 @@
 from data import *
 
-invskillnames = {y.upper():x for x,y in skillnames.items()}
 
-run = True
-while run:
-    query = input(">> ")
-    if query.lower() == "quit":
-        run = False
-    elif query.upper() in skillnames:
-        print(skillnames[query.upper()])
-    elif query.upper() in  invskillnames:
-        print(invskillnames[query.upper()])
+def skilltrans():
+    invskillnames = {y.upper():x for x,y in skillnames.items()}
+
+    run = True
+    while run:
+        query = input(">> ")
+        if query.lower() == "quit":
+            run = False
+        elif query.upper() in skillnames:
+            print(skillnames[query.upper()])
+        elif query.upper() in  invskillnames:
+            print(invskillnames[query.upper()])
