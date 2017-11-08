38611df5edb64a5f89fe34b248c889c899560203
Othko97 <othko97@gmail.com>
2017-11-08 12:54:44 +0000

    Add loading existing character

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 #########
 
 import os
+import pickle
 from char import *
 from chargen import *
 from data import *
@@ -18,14 +19,44 @@ from skilltranslator import *
 def app():
     run = True
     curchar = None
+
     while run == True:
-        command = input(">>> ").upper()
+        command = input("> ").upper()
+
         if command == "QUIT":
             run = False
+
         elif command == "NEW":
             curchar = createchar()
+
         elif command == "SKILL":
             skilltrans()
+
         elif command == "TRAIT":
             traittrans()
+
         elif command == "LOAD":
+            cwd = os.getcwd()
+            dirname = os.path.join(cwd, 'characters', 'storage')
+            chars = os.listdir(dirname)
+            print("\nAvailable Characters:")
+            for i in range(len(chars)):
+                chars[i] = chars[i].split('.')[0]
+                print(chars[i])
+            print()
+            name = input("Name: ")
+            if name in chars:
+                curchar = pickle.load(open(os.path.join(dirname, name + '.py'), 'rb'))
+            else:
+                print('No such Character\n')
+        
+        elif command == "PRINT":
+            if curchar != None:
+                curchar.output()
+        
+
+
+app()
+            
+
+            
