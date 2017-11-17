75dafe7e4872120325cea22b745e4874ecf10669
Thomas Coldrick <othko97@gmail.com>
2017-11-17 15:28:03 +0000

    Add boilerplate level up

---
+++ level-up.py
@@ -0,0 +1,14 @@
+"""System to level up a character"""
+
+#########
+#IMPORTS#
+#########
+from char import *
+from data import *
+
+###################
+#LEVEL UP FUNCTION#
+###################
+
+def level-up(char):
+  
