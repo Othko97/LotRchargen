6446d7ad133f8c09e2b4262e798579a3962a93d3
Thomas Coldrick <othko97@gmail.com>
2017-11-18 09:28:18 +0000

    Add docstrings

--- functions.py
+++ functions.py
@@ -11,31 +11,25 @@ import random as R
 ###########
 
 #ROLL 2 D6
-	#Simulates rolling of 2d6
-  #Returns integer
-
 def twoDsix():
+	"""Simulates the rolling of 2d6
+		 Returns an int in the range (1, 12)"""
 	x = R.randint(1,6)
 	y = R.randint(1,6)
-	return(x+y)
+	return x+y
 
 
 #MATRIX ADDITION FOR LISTS
-	#Adds two lists together elementwise
-  #Returns list
-
 def add(list1, list2):
-	list = []
-	for i in range(len(list1)):
-		list.append(list1[i]+list2[i])
-	return(list)
+	"""Element-wise addition on lists
+		 Returns a list"""
+	return [i1 + i2 for i1,i2 in zip(list1, list2)]
 
 
 #ATTRIBUTE MODIFIERS
-	#Calculates attribute modifiers for a given set of attributes
-	#Returns list of modifiers
-
 def attrmod(list):
+	"""Calculates modifiers for a list of attributes
+		 Returns a list of modifiers"""
 	out = []
 	for i in list:
 		if i in [0,1]:
@@ -51,16 +45,14 @@ def attrmod(list):
 	return(out)
 
 #EQUAL SPACED PRINTING
-  #Spaces lists of data into columns
-  #Prints a list of strings ready to be printed line by line
-
 def evenspace(header, buffer):
+	"""Spaces data evenly into columns for pretty printing
+		 Prints a table, returns nothing"""
   buffer.insert(0, header)
   numcols = len(header)
-  print(numcols)
-  col_widths = [(max(len(row[i]) for row in buffer) + 2) for i in range(numcols)]
+  col_widths = [(max(len(str(row[i])) for row in buffer) + 2) for i in range(numcols)]
   for row in buffer:
     printline = ""
     for i in range(numcols):
-      printline += row[i].ljust(col_widths[i])
+      printline += str(row[i]).ljust(col_widths[i])
     print(printline)
